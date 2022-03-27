4df27f8e4b233e1e6beb8bf65a94e8c32133a69e
unknown <aleksander.marciniak@gmail.com>
2022-03-27 19:35:53 +0200

    palindrom cz1

---
+++ palindrom.py
@@ -0,0 +1,6 @@
+def check(string):
+    if string[::-1]!=string: #sprawdzenie stringa z odbiciem lustrzanym
+        return False        #zwraca falsz jesli warunek nie spelniony
+    else:
+        return True
+        
