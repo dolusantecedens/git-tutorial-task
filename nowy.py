a8e3183b303acfdbb7108c31d6477930ae85d617
unknown <aleksander.marciniak@gmail.com>
2022-03-27 19:17:07 +0200

    pierwsza zmiana

--- nowy.py
+++ nowy.py
@@ -1,3 +1,7 @@
-def palindrom(string):
-    if string != string[::-1]:
-        return False
+list_length=0
+print('prezentuje liste zakupow')
+list={'piekarnia':['Chleb', 'Pączek', 'Bułki'], 'warzywniak':['marchew','seler','rukola']}
+for i in list:
+    print('Idę do  %s, i kupuje %s :'%(i.capitalize(),list[i]))
+    list_length+=len(list[i])
+print('kupujesz %i produktów' %(list_length))
